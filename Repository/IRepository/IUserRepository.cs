252bf0534751d7711b906acad55a48bd8b8d1277
DESKTOP-SE6MT31\Tihomir <tihomirascic18@gmail.com>
2024-04-27 21:33:57 +0200

    Added new IUserRepository.

---
+++ Repository/IRepository/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using TihomirsBakery.Models.User;
+
+namespace TihomirsBakery.Repository.IRepository
+{
+    public interface IUserRepository : IGenericRepository<User>
+    {
+        public Task<User> GetById(CancellationToken cancellationToken, int id);
+        public Task<User> GetByUserNameAsync(CancellationToken cancellationToken, string userName);
+        public Task<IEnumerable<User>> GetByFirstNameAsync(CancellationToken cancellationToken, string firstName);
+        public Task<IEnumerable<User>> GetByLastNameAsync(CancellationToken cancellationToken, string lastName);
+        public Task<User> GetByEmailAsync(CancellationToken cancellationToken, string email);
+        public Task<User> GetByPhoneNumberAsync(CancellationToken cancellationToken, string phoneNumber);
+        public Task<User> GetByRefreshToken(CancellationToken cancellationToken, string refreshToken);
+    }
+}
