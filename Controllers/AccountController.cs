21f1e2565b2b426a8f2a4bdf03f5458715f5e656
DESKTOP-SE6MT31\Tihomir <tihomirascic18@gmail.com>
2024-04-27 21:22:42 +0200

    Added AccountController.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TihomirsBakery.Models.Account;
+using TihomirsBakery.Models.User;
+using TihomirsBakery.Services.IServices;
+
+namespace TihomirsBakery.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private IUserService _userService;
+        private IAccountService _accountService;
+
+        public AccountController(
+            IUserService userService, 
+            IAccountService accountService)
+        {
+            _userService = userService;
+            _accountService = accountService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Login(CancellationToken cancellationToken, UserLoginRequest userToLogin)
+        {
+            try
+            {
+                var result = await _accountService.Login(cancellationToken, userToLogin);
+                return Ok(result);
+            } 
+            catch (Exception ex) 
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Refresh(CancellationToken cancellationToken, RefreshRequest model)
+        {
+            try
+            {
+                var result = await _accountService.Refresh(cancellationToken, model);
+                return Ok(result);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> ChangePassword(CancellationToken cancellationToken, ChangePasswordRequest model)
+        {
+            try
+            {
+                var result = await _accountService.ChangePassword(cancellationToken, model);
+                return Ok(result);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> ChangeUsername(CancellationToken cancellationToken, ChangeUsernameRequest model)
+        {
+            try
+            {
+                var result = await _accountService.ChangeUsername(cancellationToken, model);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Register(CancellationToken cancellationToken, UserCreateRequest model)
+        {
+            try
+            {
+                var result = await _accountService.Register(cancellationToken, model);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+    }
+}
