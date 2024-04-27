b2d46e83cc862c762c04d541db632c6442da9b4c
DESKTOP-SE6MT31\Tihomir <tihomirascic18@gmail.com>
2024-04-27 21:26:54 +0200

    Added ChangePasswordRequest model.

---
+++ Models/Account/ChangePasswordRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TihomirsBakery.Models.Account
+{
+    public class ChangePasswordRequest
+    {
+        [Required]
+        public string? CurrentPassword { get; set; }
+
+        [Required]
+        public string? NewPassword { get; set; }
+
+        [Required]
+        public string? ConfirmNewPassword { get; set; }
+    }
+}
