16d0fc473476e5e1c623f22c5d964f4b3f951b9b
DESKTOP-SE6MT31\Tihomir <tihomirascic18@gmail.com>
2024-04-27 21:27:10 +0200

    Added ChangeUsernameRequest model.

---
+++ Models/Account/ChangeUsernameRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TihomirsBakery.Models.Account
+{
+    public class ChangeUsernameRequest
+    {
+        [Required]
+        public string? NewUsername { get; set; }
+
+        [Required]
+        public string? Password { get; set; }
+    }
+}
