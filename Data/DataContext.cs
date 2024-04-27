9c1ad0e95387aecc4e10ccc310bd4c6058968dec
DESKTOP-SE6MT31\Tihomir <tihomirascic18@gmail.com>
2024-04-27 21:24:45 +0200

    Inheriting new DbContext. Added Users DbSet.

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,21 +1,26 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using TihomirsBakery.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using TihomirsBakery.Models.Meal;
+using TihomirsBakery.Models.User;
 
 namespace TihomirsBakery.Data
 {
-	public class DataContext : DbContext, IDataContext
+	public class DataContext : IdentityDbContext<User, IdentityRole<int>, int>, IDataContext
 	{
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
 				
         }
 
-		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        public DbSet<Meal> Meals { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
 			base.OnConfiguring(optionsBuilder);
 			optionsBuilder.UseNpgsql("Server=localhost;Database=mydatabase2;Port=5432;User Id=myuser2;Password=mypassword2;Pooling=true;Maximum Pool Size=3;Minimum Pool Size=1;");
 		}
-
-		public DbSet<Meal> Meals { get; set; }
 	}
 }
