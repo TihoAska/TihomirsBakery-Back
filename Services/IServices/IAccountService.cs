645a7750675b527c171e8c42f8a8f651d80daf7e
DESKTOP-SE6MT31\Tihomir <tihomirascic18@gmail.com>
2024-04-27 21:34:58 +0200

    Added new IAccountRepository.

---
+++ Services/IServices/IAccountService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TihomirsBakery.Models.Account;
+using TihomirsBakery.Models.User;
+
+namespace TihomirsBakery.Services.IServices
+{
+    public interface IAccountService
+    {
+        Task<AuthResponse> Login(CancellationToken cancellationToken, UserLoginRequest userToLogin);
+        Task<AuthResponse> Refresh(CancellationToken cancellationToken, RefreshRequest model);
+        Task<IdentityResult> ChangePassword(CancellationToken cancellationToken, ChangePasswordRequest model);
+        Task<IdentityResult> ChangeUsername(CancellationToken cancellationToken, ChangeUsernameRequest model);
+        Task<AuthResponse> Register(CancellationToken cancellationToken, UserCreateRequest userForRegistration);
+        Task<IdentityResult> DeleteAccount(string password);
+    }
+}
