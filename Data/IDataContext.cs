6795fc46ab30b09fbc9662fd53235bd48216d44f
DESKTOP-SE6MT31\Tihomir <tihomirascic18@gmail.com>
2024-04-27 21:25:09 +0200

    Added Users DbSet. Updated using directive.

--- Data/IDataContext.cs
+++ Data/IDataContext.cs
@@ -1,12 +1,14 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
-using TihomirsBakery.Models;
+using TihomirsBakery.Models.Meal;
+using TihomirsBakery.Models.User;
 
 namespace TihomirsBakery.Data
 {
     public interface IDataContext : IDisposable
     {
         public DbSet<Meal> Meals { get; set; }
+        public DbSet<User> Users { get; set; }
         
         Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
         DbSet<TEntity> Set<TEntity>() where TEntity : class;
