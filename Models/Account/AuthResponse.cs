4879a2854cb20252ae17986e90e5a090d55e1d59
DESKTOP-SE6MT31\Tihomir <tihomirascic18@gmail.com>
2024-04-27 21:26:38 +0200

    Added AuthResponse model.

---
+++ Models/Account/AuthResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace TihomirsBakery.Models.Account
+{
+    public class AuthResponse
+    {
+        public bool IsAuthSuccessful { get; set; }
+        public string? ErrorMessage { get; set; }
+        public string? AccessToken { get; set; }
+        public string? RefreshToken { get; set; }
+    }
+}
