16815d0895b155247d0ee75ea849ac42cad80a1d
DESKTOP-SE6MT31\Tihomir <tihomirascic18@gmail.com>
2024-04-30 19:21:58 +0200

    Removed unnecessary .ForMember functions.

--- Helpers/AutoMapperProfile.cs
+++ Helpers/AutoMapperProfile.cs
@@ -8,35 +8,13 @@ namespace TihomirsBakery.Helpers
     {
         public AutoMapperProfile()
         {
-            CreateMap<MealCreateRequest, Meal>()
-                .ForMember(d => d.Name, opt => opt.MapFrom(s => s.Name))
-                .ForMember(d => d.Description, opt => opt.MapFrom(s => s.Description))
-                .ForMember(d => d.Type, opt => opt.MapFrom(s => s.Type))
-                .ForMember(d => d.Proteins, opt => opt.MapFrom(s => s.Proteins))
-                .ForMember(d => d.Fats, opt => opt.MapFrom(s => s.Fats))
-                .ForMember(d => d.Carbs, opt => opt.MapFrom(s => s.Carbs));
+            CreateMap<MealCreateRequest, Meal>();
 
-            CreateMap<MealUpdateRequest, Meal>()
-                .ForMember(d => d.Id, opt => opt.MapFrom(s => s.Id))
-                .ForMember(d => d.Name, opt => opt.MapFrom(s => s.Name))
-                .ForMember(d => d.Description, opt => opt.MapFrom(s => s.Description))
-                .ForMember(d => d.Type, opt => opt.MapFrom(s => s.Type))
-                .ForMember(d => d.Proteins, opt => opt.MapFrom(s => s.Proteins))
-                .ForMember(d => d.Fats, opt => opt.MapFrom(s => s.Fats))
-                .ForMember(d => d.Carbs, opt => opt.MapFrom(s => s.Carbs));
+            CreateMap<MealUpdateRequest, Meal>();
 
-            CreateMap<UserCreateRequest, User>()
-                .ForMember(d => d.FirstName, opt => opt.MapFrom(s => s.FirstName))
-                .ForMember(d => d.LastName, opt => opt.MapFrom(s => s.LastName))
-                .ForMember(d => d.UserName, opt => opt.MapFrom(s => s.UserName))
-                .ForMember(d => d.Email, opt => opt.MapFrom(s => s.Email));
+            CreateMap<UserCreateRequest, User>();
 
-            CreateMap<UserUpdateRequest, User>()
-                .ForMember(d => d.Id, opt => opt.MapFrom(s => s.Id))
-                .ForMember(d => d.FirstName, opt => opt.MapFrom(s => s.FirstName))
-                .ForMember(d => d.LastName, opt => opt.MapFrom(s => s.LastName))
-                .ForMember(d => d.UserName, opt => opt.MapFrom(s => s.UserName))
-                .ForMember(d => d.Email, opt => opt.MapFrom(s => s.Email));
+            CreateMap<UserUpdateRequest, User>();
         }
     }
 }
