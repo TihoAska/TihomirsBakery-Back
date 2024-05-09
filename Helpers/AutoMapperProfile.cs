10b648b5eb9084a7503d0931882983f4db088fe0
DESKTOP-SE6MT31\Tihomir <tihomirascic18@gmail.com>
2024-05-09 18:43:29 +0200

    Mapped MealIntake to requests.

--- Helpers/AutoMapperProfile.cs
+++ Helpers/AutoMapperProfile.cs
@@ -1,5 +1,7 @@
 using AutoMapper;
 using TihomirsBakery.Models.Meals;
+using TihomirsBakery.Models.Nutritions;
+using TihomirsBakery.Models.Nutritions.AddedMeals;
 using TihomirsBakery.Models.Users;
 
 namespace TihomirsBakery.Helpers
@@ -15,6 +17,32 @@ namespace TihomirsBakery.Helpers
             CreateMap<UserCreateRequest, User>();
 
             CreateMap<UserUpdateRequest, User>();
+
+            CreateMap<MealIntakeCreateRequest, MealIntake>()
+                .ForMember(d => d.AddedMeals, opt => opt.MapFrom(s => s.AddedMeals.Select(am => new AddedMeal()
+                {
+                    Name = am.Name,
+                    MealType = am.MealType,
+                    MealIntakeId = am.MealIntakeId,
+                    Calories = am.Calories,
+                    Carbs = am.Carbs,
+                    Protein = am.Protein,
+                    Fats = am.Fats,
+                })));
+
+            CreateMap<MealIntakeUpdateRequest, MealIntake>()
+                .ForMember(d => d.AddedMeals, opt => opt.MapFrom(s => s.AddedMeals.Select(am => new AddedMeal()
+                {
+                    Name = am.Name,
+                    MealType = am.MealType,
+                    MealIntakeId = am.MealIntakeId,
+                    Calories = am.Calories,
+                    Carbs = am.Carbs,
+                    Protein = am.Protein,
+                    Fats = am.Fats,
+                })));
+
+            CreateMap<AddedMealCreateRequest, AddedMeal>();
         }
     }
 }
