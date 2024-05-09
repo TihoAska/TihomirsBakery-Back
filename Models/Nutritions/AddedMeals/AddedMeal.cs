45329dc14ad10a33091cd4c71d79d5b94cdd7351
DESKTOP-SE6MT31\Tihomir <tihomirascic18@gmail.com>
2024-05-09 18:45:07 +0200

    New AddedMeal model.

---
+++ Models/Nutritions/AddedMeals/AddedMeal.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TihomirsBakery.Models.Nutritions.AddedMeals
+{
+    public class AddedMeal
+    {
+        public int Id { get; set; }
+        public int MealIntakeId { get; set; }
+
+        [ForeignKey("MealIntakeId")]
+        public MealIntake MealIntake { get; set; }
+        public MealType MealType { get; set; }
+        public string? Name { get; set; }
+        public double Calories { get; set; }
+        public double Protein { get; set; }
+        public double Carbs { get; set; }
+        public double Fats { get; set; }
+    }
+}
---
+++ Models/Nutritions/AddedMeals/AddedMealCreateRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace TihomirsBakery.Models.Nutritions.AddedMeals
+{
+    public class AddedMealCreateRequest
+    {
+        public string Name { get; set; }
+        public int MealIntakeId { get; set; }
+        public MealType MealType { get; set; }
+        public double Calories { get; set; }
+        public double Protein { get; set; }
+        public double Carbs { get; set; }
+        public double Fats { get; set; }
+    }
+}
