71d6daacd2e492196ad92ee747362182f71c5991
DESKTOP-SE6MT31\Tihomir <tihomirascic18@gmail.com>
2024-05-09 18:54:38 +0200

    Added DailyIntakeService.

---
+++ Services/DailyIntakeService.cs
@@ -0,0 +1,26 @@
+﻿using TihomirsBakery.Models.Nutritions;
+using TihomirsBakery.Repository.IRepository;
+using TihomirsBakery.Services.IServices;
+
+namespace TihomirsBakery.Services
+{
+    public class DailyIntakeService : IDailyIntakeService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DailyIntakeService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<DailyIntake> Get(CancellationToken cancellationToken)
+        {
+            return await _unitOfWork.DailyIntakes.GetByDateCreated(cancellationToken, DateTime.Now);
+        }
+
+        public async Task<DailyIntake> GetByUserIdForToday(CancellationToken cancellationToken, int userId)
+        {
+            return await _unitOfWork.DailyIntakes.GetByUserIdForToday(cancellationToken, userId);
+        }
+    }
+}
---
+++ Services/IServices/IDailyIntakeService.cs
@@ -0,0 +1,10 @@
+﻿using TihomirsBakery.Models.Nutritions;
+
+namespace TihomirsBakery.Services.IServices
+{
+    public interface IDailyIntakeService
+    {
+        public Task<DailyIntake> Get(CancellationToken cancellationToken);
+        public Task<DailyIntake> GetByUserIdForToday(CancellationToken cancellationToken, int userId);
+    }
+}
