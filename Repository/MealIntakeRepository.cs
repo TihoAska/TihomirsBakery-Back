53d6b4be21907b837e8604b0946a448cee35e07c
DESKTOP-SE6MT31\Tihomir <tihomirascic18@gmail.com>
2024-05-09 18:52:16 +0200

    New MealIntake repository.

---
+++ Repository/MealIntakeRepository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TihomirsBakery.Data;
+using TihomirsBakery.Models.Nutritions;
+using TihomirsBakery.Repositories;
+using TihomirsBakery.Repository.IRepository;
+
+namespace TihomirsBakery.Repository
+{
+    public class MealIntakeRepository : GenericRepository<MealIntake>, IMealIntakeRepository
+    {
+        public MealIntakeRepository(IDataContext context) : base(context)
+        {
+
+        }
+
+        public Task<List<MealIntake>> GetByDateCreated(CancellationToken cancellationToken, DateTime dateCreated)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<MealIntake> GetById(CancellationToken cancellationToken, int id)
+        {
+            return _query.Where(mi => mi.Id == id).FirstOrDefaultAsync(cancellationToken);
+        }
+
+        public Task<List<MealIntake>> GetByDailyIntakeId(CancellationToken cancellationToken, int dailyIntakeId)
+        {
+            return _query.Where(mi => mi.DailyIntakeId == dailyIntakeId).ToListAsync(cancellationToken);
+        }
+    }
+}
