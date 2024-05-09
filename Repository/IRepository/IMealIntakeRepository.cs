fdfe5237edc5292a91fbc3d31920ed9b7f6b83d4
DESKTOP-SE6MT31\Tihomir <tihomirascic18@gmail.com>
2024-05-09 18:50:07 +0200

    New MealIntake repository.

---
+++ Repository/IRepository/IMealIntakeRepository.cs
@@ -0,0 +1,11 @@
+﻿using TihomirsBakery.Models.Nutritions;
+
+namespace TihomirsBakery.Repository.IRepository
+{
+    public interface IMealIntakeRepository : IGenericRepository<MealIntake>
+    {
+        Task<MealIntake> GetById(CancellationToken cancellationToken, int id);
+        Task<List<MealIntake>> GetByDailyIntakeId(CancellationToken cancellationToken, int userId);
+        Task<List<MealIntake>> GetByDateCreated(CancellationToken cancellationToken, DateTime dateCreated);
+    }
+}
