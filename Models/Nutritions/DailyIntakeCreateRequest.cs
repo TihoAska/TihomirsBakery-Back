ca132a63d0ee3f1594f47d2b6dfff783dcf0492a
DESKTOP-SE6MT31\Tihomir <tihomirascic18@gmail.com>
2024-05-09 18:46:11 +0200

    New requests.

---
+++ Models/Nutritions/DailyIntakeCreateRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace TihomirsBakery.Models.Nutritions
+{
+    public class DailyIntakeCreateRequest
+    {
+        public double TotalCalories { get; set; }
+        public double TotalProtein { get; set; }
+        public double TotalCarbs { get; set; }
+        public double TotalFats { get; set; }
+    }
+}
---
+++ Models/Nutritions/MealIntakeUpdateRequest.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+using TihomirsBakery.Models.Nutritions.AddedMeals;
+
+namespace TihomirsBakery.Models.Nutritions
+{
+    public class MealIntakeUpdateRequest
+    {
+        public MealType MealType { get; set; }
+        public double Calories { get; set; }
+        public double Protein { get; set; }
+        public double Carbs { get; set; }
+        public double Fats { get; set; }
+
+        [JsonIgnore]
+        public List<AddedMealCreateRequest> AddedMeals { get; set; }
+    }
+}
